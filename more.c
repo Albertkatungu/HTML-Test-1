96c588384572881db39feb2d0a5500f59a20175e
Albertkatungu <albertkatungu@gmail.com>
2023-04-05 23:01:35 +0300

    HTML Test commit

---
+++ more.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Press Enter: ");
+    getchar();
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar('!');
+    putchar('w');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('\n');
+    return 0;
+}
